2662ca8a52b7801018674fedbbc82697a84a8dfe
tianhong229@gmail.com <tianhong229@gmail.com>
2018-11-20 15:18:33 +0800

    first commit

---
+++ src/main/java/com/tianhong/monitor_tuning/MonitorTuningApplication.java
@@ -0,0 +1,12 @@
+package com.tianhong.monitor_tuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorTuningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorTuningApplication.class, args);
+    }
+}
