da8c364547e9e1de4d78b38ae4df2567c916218b
tianhong229@gmail.com <tianhong229@gmail.com>
2018-11-20 15:55:20 +0800

    wip: chapter 2

---
+++ src/main/java/com/tianhong/monitor_tuning/chapter2/MemoryController.java
@@ -0,0 +1,44 @@
+package com.tianhong.monitor_tuning.chapter2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @Author: TianhongWang
+ * @Date: 2018/11/20
+ * @Description
+ */
+
+@RestController
+public class MemoryController {
+    private List<User> userList = new ArrayList<>();
+    private List<Class<?>> classList = new ArrayList<Class<?>>();
+    /**
+     *  模拟堆逸出
+     *  堆设置为32M： -Xmx32M -Xms32M
+     * @return
+     */
+    @GetMapping("/heap")
+    public String heap(){
+        int i = 0;
+        while(true){
+            userList.add(new User(i++, UUID.randomUUID().toString()));
+        }
+    }
+
+    /**
+     * -XX:MetaspaceSize=32M -XX:MaxMetaspaceSize=32M
+     * 模拟metaspace逸出
+     * @return
+     */
+    @GetMapping("/nonheap")
+    public String nonheap(){
+        while(true){
+            classList.addAll(Metaspace.createClasses());
+        }
+    }
+}
---
+++ src/main/java/com/tianhong/monitor_tuning/chapter2/User.java
@@ -0,0 +1,32 @@
+package com.tianhong.monitor_tuning.chapter2;
+
+/**
+ * @Author: TianhongWang
+ * @Date: 2018/11/20
+ * @Description
+ */
+public class User {
+    private int id;
+    private String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
