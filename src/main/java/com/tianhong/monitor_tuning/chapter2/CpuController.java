275187c0c366cc3785dc18310b17f105f4819ea2
tianhong229@gmail.com <tianhong229@gmail.com>
2018-11-28 10:58:25 +0800

    rm comments

--- src/main/java/com/tianhong/monitor_tuning/chapter2/CpuController.java
+++ src/main/java/com/tianhong/monitor_tuning/chapter2/CpuController.java
@@ -1,9 +1,71 @@
 package com.tianhong.monitor_tuning.chapter2;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: TianhongWang
  * @Date: 2018/11/21
  * @Description
  */
+
+@RestController
 public class CpuController {
+
+    @RequestMapping("/loop")
+    public List<Long> loop(){
+        String data = "{\"data\":[{\"partnerid\":,\"count\":\"10000\",\"cityid\":\"11\"},{\"partnerid\":983,\"count\":\"10000\",\"cityid\":\"11\"},{\"partnerid\":984,\"count\":\"10000\",\"cityid\":\"11\"}]}";
+        return getPartneridsFromJson(data);
+    }
+
+    public static List<Long> getPartneridsFromJson(String data){
+        //{\"data\":[{\"partnerid\":982,\"count\":\"10000\",\"cityid\":\"11\"},{\"partnerid\":983,\"count\":\"10000\",\"cityid\":\"11\"},{\"partnerid\":984,\"count\":\"10000\",\"cityid\":\"11\"}]}
+        //上面是正常的数据
+        List<Long> list = new ArrayList<Long>(2);
+        if(data == null || data.length() <= 0){
+            return list;
+        }
+        int datapos = data.indexOf("data");
+        if(datapos < 0){
+            return list;
+        }
+        int leftBracket = data.indexOf("[",datapos);
+        int rightBracket= data.indexOf("]",datapos);
+        if(leftBracket < 0 || rightBracket < 0){
+            return list;
+        }
+        String partners = data.substring(leftBracket+1,rightBracket);
+        if(partners == null || partners.length() <= 0){
+            return list;
+        }
+        while(partners!=null && partners.length() > 0){
+            int idpos = partners.indexOf("partnerid");
+            if(idpos < 0){
+                break;
+            }
+            int colonpos = partners.indexOf(":",idpos);
+            int commapos = partners.indexOf(",",idpos);
+            if(colonpos < 0 || commapos < 0){
+                //partners = partners.substring(idpos+"partnerid".length());//1
+                continue;
+            }
+            String pid = partners.substring(colonpos+1,commapos);
+            if(pid == null || pid.length() <= 0){
+                //partners = partners.substring(idpos+"partnerid".length());//2
+                continue;
+            }
+            try{
+                list.add(Long.parseLong(pid));
+            }catch(Exception e){
+                //do nothing
+            }
+            partners = partners.substring(commapos);
+        }
+        return list;
+    }
+
+
 }
---
+++ src/main/java/com/tianhong/monitor_tuning/test.java
@@ -0,0 +1,17 @@
+package com.tianhong.monitor_tuning;
+
+/**
+ * @Author: TianhongWang
+ * @Date: 2018/11/26
+ * @Description
+ */
+public class test {
+    public static void main(String[] args) {
+        Integer x = Integer.valueOf(123);
+        Integer y = Integer.valueOf(123);
+        String test = new String("ertw");
+        System.out.println(test);
+        System.out.println(x == y);
+
+    }
+}
